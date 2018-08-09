465ff4381f15980114691534cfc6c99ca2b6fb80
Turbosaurio <reinholandia@hotmail.com>
2018-08-09 08:09:59 -0500

    change book shelves option to do stuff on change of the value instead of a click

--- reactnd-project-myreads-starter/src/books_components/BookSelect.js
+++ reactnd-project-myreads-starter/src/books_components/BookSelect.js
@@ -1,35 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 // import * as  BooksAPI from '../BooksAPI'
 
-export default function BookSelect (props){
-	let {moveBook} = props
-	let {id, shelf} = props.book_data
-	let options = [
-		{label: "Currently Reading", shelfKey: "currentlyReading"},
-		{label: "Want to Read", shelfKey: "wantToRead"},
-		{label: "Read", shelfKey: 'read'},
-		{label: "None", shelfKey: 'none'},
-	]
-	return (
-		<div className="book-shelf-changer">
-			<select defaultValue={() => {
-				for(let option of options){
-					if (option.shelfKey === shelf){
-						return shelf
-					}
-				}
-			}}>
-				<option disabled>Move to...</option>
-				{options.map((option, index) => {
-					
-						if(shelf === option.shelfKey){
-							return <option value={option.shelfKey} key={index}>&#9755; {option.label}</option>
-						} else{
-							return <option value={option.shelfKey} key={index} onClick={()=> moveBook(id, option.shelfKey) }>{option.label}</option>							
-						}
-					
-				})}
-			</select>
-		</div>
-	)
+export default class BookSelect extends Component{
+
+
+	handleShelf = event =>{
+		this.props.moveBook(this.props.book_data.id, event.target.value)
+	}
+	
+	render(){
+		let options = [
+			{label: "Currently Reading", shelfKey: "currentlyReading"},
+			{label: "Want to Read", shelfKey: "wantToRead"},
+			{label: "Read", shelfKey: 'read'},
+			{label: "None", shelfKey: 'none'},
+		]
+
+
+		return (
+			<div className="book-shelf-changer">
+				<select
+					defaultValue={this.props.book_data.shelf ? this.props.book_data.shelf : 'none'}
+					onChange={this.handleShelf}
+				>
+					<option disabled>Move to...</option>
+					{options.map((option, index) => <option value={option.shelfKey} key={index}>{option.label}</option>)}
+				</select>
+			</div>
+		)
+	}
 }
